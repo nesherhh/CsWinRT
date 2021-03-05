1512e5bd9beb37d1ef52b54f65b343f1431df5bc
Joshua Larkin <jlarkin@microsoft.com>
2021-03-05 14:04:20 -0800

    move to new winui and vsix

---
+++ src/Samples/BgTaskComponent/WinUIApp/WinUIApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.UI.Xaml;
+using Windows.ApplicationModel.Background;
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUIApp
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var taskRegistered = false;
+            var exampleTaskName = "ToastBgTask";
+
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == exampleTaskName)
+                {
+                    taskRegistered = true;
+                    break;
+                }
+            }
+
+            if (!taskRegistered)
+            {
+                var builder = new BackgroundTaskBuilder
+                {
+                    Name = exampleTaskName,
+                    TaskEntryPoint = "BgTaskComponent.ToastBgTask"
+                };
+                builder.SetTrigger(new SystemTrigger(SystemTriggerType.TimeZoneChange, false));
+                builder.Register();
+            }
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+    }
+}
---
+++ src/Samples/BgTaskComponent/clean.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rd /q/s BgTaskComponent\bin BgTaskComponent\obj
+
+rd /q/s WpfApp\bin WpfApp\obj
+
+rd /q/s WpfApp.Package\bin WpfApp.Package\obj WpfApp.Package\AppPackages 
